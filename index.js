2f850aacdc6eb36596f95e2632be630c04f58622
Emily Asch <e.asch@me.com>
2018-07-20 16:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var height = "74"
 
 if (name === "Susan"){
   console.log("The name is correct")
-} else {
-  console.log
+} 
+if (height === "74"){
+  console.log("The height is correct")
 }
 
 // Don't worry about this
